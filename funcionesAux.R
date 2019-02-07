c7ab17ae57b8863fb29c47f38f2b2dbdae427480
Samuel Cardenete <samuel@cvi091103.ugr.es>
2019-02-07 13:55:12 +0100

    ruido, reduccion. primer modelo RIPPER

--- funcionesAux.R
+++ funcionesAux.R
@@ -10,11 +10,23 @@ generarEnvio = function(y,file="envio.csv",path="./"){
   # se compone el path completo
   pathCompleto <- paste(path,file,sep="")
   
-  ids <- 1:length(y)
-  prediccion <- as.vector(y)
-  res <- as.data.frame(cbind(ids,prediccion))
+  Id <- 1:length(y)
+  Prediction <- as.vector(y)
+  res <- as.data.frame(cbind(Id,Prediction))
   
   write.csv(res,file = pathCompleto,row.names=FALSE,quote=FALSE)
   
   
+}
+
+
+# funcion para generar el archivo con el envio a kaggle. Argumentos:
+# @param predictionLabels etiquetas predichas por el modelo de prediccion
+# @param testLabels etiquetas de test
+# @output precision
+
+precision = function(predictionLabels, testLabels){
+  
+  tabla = table(predictionLabels,testLabels)
+  (tabla[1,1] + tabla[2,2] )/ sum(tabla)
 }
